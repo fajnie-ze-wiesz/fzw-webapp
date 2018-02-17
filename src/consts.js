d2a003b7fd0a73478da8c1731fcab6470dba2871
Andrzej Pragacz <apragacz@o2.pl>
2018-02-17 23:24:19 +0100

    negative types

--- src/consts.js
+++ src/consts.js
@@ -1,2 +1,8 @@
 export const QUESTION_TIMEOUT = 5 * 1000;
 export const NUM_OF_QUIZ_QUESTIONS = 10;
+export const NEGATIVE_TYPES = [
+  'clickbait',
+  'fake-news',
+  'image-manipulation',
+  'emotional-language',
+];
