78b1d8621c4900b107671c536b1d021b175a622e
Michał Wołoszyn <fraunos@gmail.com>
2018-02-18 12:02:48 +0100

    statistics circle

--- src/components/PieChart2.vue
+++ src/components/PieChart2.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
       <g>
         <!-- <path :d="path" stroke="black" fill="transparent"/> -->
-        <polyline :points="points" style="fill:none;stroke:black;stroke-width:3" />
+        <text x="25" y="28" font-size="10" text-anchor="middle" dominant-baseline="center">{{Math.floor(this.score/this.total*100)}}%</text>
+        <polyline :points="points" style="fill:none;stroke:white;stroke-width:2" />
+        <polyline :points="points2" style="fill:none;stroke:gray;stroke-width:2" />
       </g>
     </svg>
   </div>
@@ -12,21 +14,31 @@
 <script>
   export default {
     name: 'pie-chart2',
-    props: ['time', 'label'],
+    props: ['score', 'total'],
     data () {
       return {
         path: '',
-        points: '20,20 40,25 60,40 80,120 120,140 200,180'
+        points: '',
+        points2: ''
       }
     },
     created () {
-      this.points = this.generateCircle(this.time)
+      this.generateCircle(this.score, this.total)
     },
     methods: {
-      generateCircle (n) {
-        // let x, y
-        for (var i = 0; i < n; i++) {
-
+      generateCircle (s, n) {
+        let r = 100 / n
+        n = 100
+        s *= r
+        let x, y, angle
+        for (var i = 0; i < n + 2; i++) {
+          angle = Math.PI * 2 / n * i
+          x = 25 + 15 * Math.cos(angle + Math.PI / 2)
+          y = 25 + 15 * -Math.sin(angle + Math.PI / 2)
+          this.points += `${x},${y} `
+          if (s >= i) {
+            this.points2 = this.points
+          }
         }
       }
     }
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -2,26 +2,16 @@
   <div id="results">
     <h1>{{this.userInfo.name}}</h1>
     <h2>Twoja ochrona jest na poziomie</h2>
-    <svg viewBox="0 0 500 300">
-      <path id="mpath" fill="none" stroke="#000000" stroke-miterlimit="10" d="M34,132.5c13.8,5.6,21.7,13.2,30,24.9c10.2,14.2,28.1,28,43.8,35.1
-    	c23,10.5,47.2,2.3,65.8-13.6c13.4-11.4,30.7-24.5,49.2-24.6c18.9-0.1,37,16.4,52.5,26c15.3,9.5,30.1,19.8,46.6,26.7
-    	c27.9,11.7,56.6-9.5,76-27.6c12-11.2,19-26,29.1-39.1c3.8-5,7-10.7,11.2-14.6c2-1.9,4-3.9,5.9-6c2.2-2.3,5.7-3.8,6.7-6
-    	c9.6-3.9,20.1-10.1,30.9-9.7"/>
-      <circle r="10" fill="orange">
-    <animateMotion dur="5s" repeatCount="indefinite">
-      <mpath xlink:href="#mpath" />
-    </animateMotion>
-  </circle>
-</svg>
 
     <h2>Twoja odporność na:</h2>
-    <pie-chart2></pie-chart2>
-
-
+    <pie-chart2 :score="numOfCorrectAnswers" :total="numOfAnswers"></pie-chart2>
     <p>
-      Odpowiedziałeś poprawne na {{ numOfCorrectAnswers }} pytań z {{ numOfAnswers }}.
-      Na {{ numOfOmmitedQuestions }} nie udzieliłeś odpowiedzi.
+      Udzielono {{ numOfCorrectAnswers }} poprawnych odpowiedzi na {{ numOfAnswers }} pytań.
+      Na {{ numOfOmmitedQuestions }} nie udzielono odpowiedzi.
     </p>
+    <h3>Chcesz wiedzieć więcej?</h3>
+    <h2>Odwiedź Akademię Fact-Checkingu!</h2>
+    <a class="button" href="https://www.facebook.com/akademiafactcheckingu/">przejdź</a>
   </div>
 </template>
 
@@ -47,6 +37,8 @@
         return this.$store.state.userInfo
       }
     },
+    methods: {
+    }
   }
 </script>
 
