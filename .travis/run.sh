2c9770c53acefbe2da7321338183f993c262567f
Andrzej Pragacz <apragacz@o2.pl>
2018-03-31 16:57:47 +0200

    Initial Travis config

---
+++ .travis/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly SCRIPT_NAME="$( basename "${BASH_SOURCE[0]}" )"
+readonly BASE_DIR="$( dirname "${SCRIPT_DIR}" )"
+
+print_step() {
+    echo ""
+    echo "[${SCRIPT_NAME}] $1"
+}
+
+main() {
+    print_step "Running npm run lint"
+    npm run lint
+    print_step "Running npm run build"
+    npm run build
+}
+
+main
