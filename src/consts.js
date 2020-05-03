24dd7b8a82c242874af7674a025bba2ab60b9133
Andrzej Pragacz <apragacz@o2.pl>
2020-05-03 13:52:51 +0200

    Remove MANIPULATION_CATEGORY_NAMES

--- src/consts.js
+++ src/consts.js
@@ -1,8 +1,2 @@
 export const QUESTION_TIMEOUT = 10 * 1000;
 export const NUM_OF_QUIZ_QUESTIONS = 16;
-export const MANIPULATION_CATEGORY_NAMES = [
-  'clickbait',
-  'fake-news',
-  'image-manipulation',
-  'emotional-language',
-];
