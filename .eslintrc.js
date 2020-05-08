526aaed87ecbb0da3c85459ae3d9304e6d78d3c4
Andrzej Pragacz <apragacz@o2.pl>
2020-05-09 01:11:33 +0200

    Update eslint config
    Switch specific rules to warnings to speed-up development.
    dev server will not stop building because of trivial stuff.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,31 +4,81 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    sourceType: 'module',
   },
   env: {
     browser: true,
   },
+
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
+
   // required to lint *.vue files
   plugins: [
-    'html'
+    'html',
   ],
+
   // add your custom rules here
+
   'rules': {
-    'semi': 0,
-    'comma-dangle': 0,
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    "space-before-function-paren": ["error", {
-      "anonymous": "always",
-      "named": "ignore",
-      "asyncArrow": "always"
+    /*
+     * set rules not breaking the JS code (i.e. formatting ones) to
+     * warning level instead of error level to avoid the
+     * annoying dev server "error screen" in browser (the warnings will still
+     * appear in dev server CLI output).
+     */
+
+    // set max number of chars in a line to 100; ignore lines with urls
+    'max-len': ['warn', {
+      'code': 100,
+      'ignoreComments': false,
+      'ignoreTrailingComments': false,
+      'ignoreUrls': true,
+      'ignoreStrings': false,
+      'ignoreTemplateLiterals': false,
+      'ignoreRegExpLiterals': false,
     }],
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
-}
+
+    // force semicolons
+    'semi': ['warn', 'always', {
+      'omitLastInOneLineBlock': false,
+    }],
+
+    // spaces before/after if/for/while
+    'keyword-spacing': ['warn', {
+      'before': true,
+      'after': true,
+    }],
+
+    // force dangling commas in multiline dicts/arrays/function calls
+    'comma-dangle': ['warn', 'always-multiline'],
+
+    // do not allow paren-less arrow functions
+    'arrow-parens': ['warn', 'always'],
+
+    // allow async-await
+    'generator-star-spacing': ['warn', {
+      'before': true,
+      'after': false,
+    }],
+
+    // force spaces before argument list except of named functions
+    'space-before-function-paren': ['warn', {
+      'anonymous': 'always',
+      // named functions should not have separating space
+      // e.g. `function someFun() {}`
+      'named': 'never',
+      'asyncArrow': 'always',
+    }],
+
+    // operators like '+' should be spaced appropriately
+    'space-infix-ops': ['warn', { 'int32Hint': false }],
+
+    /*
+     * remaining rules
+     */
+
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
