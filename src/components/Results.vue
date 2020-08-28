37bdc1495e1ab0378598698a064f40399d4fc895
Andrzej Pragacz <apragacz@o2.pl>
2020-08-29 00:04:59 +0200

    Update results

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,36 +1,17 @@
 <template>
   <div id="results">
     <div class="">
-      <h1>Twoja odporność na fake news to {{ correctnessPercent }}%</h1>
+      <img
+        class="jumbotron"
+        src="static/img/result.png"
+        alt="source"
+      >
+      <h1>Dobrze ci poszło</h1>
       <p>
-        Udało Ci się odpowiedzieć prawidłowo na {{ numOfCorrectAnswers }}
-        z {{ numOfQuestions }} pytań.
-      </p>
-      <p v-if="correctnessPercent == 100">
-        Świetna robota! Na początku oceniłeś się na {{ correctnessEstimatePercent }}%,
-        dlatego powinieneś być z siebie dumny.
-      </p>
-      <p v-else-if="correctnessBetterThanEstimated && correctnessPercent < 50">
-        Jest lepiej niż Ci się wydaje, ale potrzebujesz jeszcze treningu.
-        Spróbuj zagrać jeszcze raz!
-      </p>
-      <p v-else-if="correctnessWorseThanEstimated && correctnessPercent < 50">
-        Zdecydowanie powinieneś zagrać jeszcze raz i poprawić swoją odporność na fake newsy.
-        Tym bardziej, że oceniłeś się na początku na {{ correctnessEstimatePercent }}%.
-      </p>
-      <p v-else-if="correctnessBetterThanEstimated && correctnessPercent > 50">
-        Dobra robota, jesteś już prawie odporny na fake newsy. Na początku oceniłeś
-        swoją odporność na {{ correctnessEstimatePercent }}%, może kolejnym razem pójdzie Ci
-        jeszcze lepiej.
+        {{ numOfCorrectAnswers }} z {{ numOfQuestions }} dobrych odpowiedzi
       </p>
       <p>
-        W ocenie braliśmy pod uwagę:
-        <ul>
-          <li>rozpoznawanie fałszywej treści</li>
-          <li>znajdowanie fałszywego kontekstu</li>
-          <li>zauważanie manipulacji obrazami</li>
-          <li>wyłapanie clickbaitu</li>
-        </ul>
+        Jeszcze trochę i mistrzowsko będziesz rozbrajał fake newsy!
       </p>
     </div>
     <router-link
@@ -40,7 +21,7 @@
       class="red"
       to="/answers-review"
     >
-      Sprawdź swoje odpowiedzi
+      Sprawdź odpowiedzi
     </router-link>
   </div>
 </template>
@@ -160,6 +141,9 @@ export default {
     overflow: auto;
     background: var(--color-blue);
     color: white;
+  }
+  img.jumbotron {
+    width: 100%;
   }
   ul {
       padding-left: 1em;
