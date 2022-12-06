13a6a7636b235e759e03c801e956d7c858075dfa
Andrzej Pragacz <apragacz@o2.pl>
2022-12-06 01:55:12 +0100

    Update backend

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: '"production"',
   BUILD_ENV: '"production"',
   GOOGLE_ANALYTICS_TRACKER_ID: '"UA-156092120-1"',
-  BACKEND_HOST: '"fzw-backend.herokuapp.com"',
+  BACKEND_HOST: '"backend.fakesnothanks.pl"',
   BACKEND_HTTP_SCHEME: '"https"',
   APP_HOST: '"aplikacja.fajniezewiesz.pl"',
   APP_HTTP_SCHEME: '"https"',
--- config/staging.env.js
+++ config/staging.env.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: '"production"',
   BUILD_ENV: '"staging"',
   GOOGLE_ANALYTICS_TRACKER_ID: '"G-NDF450FQCH"',
-  BACKEND_HOST: '"fzw-backend.herokuapp.com"',
+  BACKEND_HOST: '"backend.fakesnothanks.pl"',
   BACKEND_HTTP_SCHEME: '"https"',
   APP_HOST: '"test-app.fajniezewiesz.pl"',
   APP_HTTP_SCHEME: '"https"',
